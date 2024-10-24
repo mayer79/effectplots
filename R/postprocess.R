0f91c3f35d2252c8ecc9ac2a59dfeb3a8403b873
Michael Mayer <mayermichael79@gmail.com>
2024-10-24 14:42:40 +0200

    implement collapse logic

--- R/postprocess.R
+++ R/postprocess.R
@@ -1,11 +1,12 @@
 #' Postprocess Output
 #'
-#' This function offers different ways to improve the output of [marginal()],
-#' [partial_dependence()], [average_observed()]. Except for `choose_stats`,
+#' This function helps to improve the output of [marginal()],
+#' [partial_dependence()], [average_observed()]. Except for `drop_stats`,
 #' all arguments are vectorized, i.e., you can select different values per X variable.
 #'
 #' @param object Object of class "marginal".
-#' @param choose_stats Statistics to select, by default `c("obs", "pred", "pd")`.
+#' @param drop_stats Statistics to drop, by default `NULL`.
+#'   Subset of "pred", "obs", "pd". Not vectorized over `object` elements.
 #' @param eval_at_center If `FALSE` (default), the points are aligned with (weighted)
 #'   average X values per bin. If `TRUE`, the points are aligned with bar centers.
 #'   Since categoricals are always evaluated at bar centers, this only affects numerics.
@@ -22,12 +23,13 @@
 #' fit <- lm(Sepal.Length ~ ., data = iris)
 #' xvars <- colnames(iris)[-1]
 #' marginal(fit, v = xvars, data = iris, y = "Sepal.Length", breaks = 5) |>
-#'   postprocess(choose_stats = "pd", eval_at_center = TRUE) |>
+#'   postprocess(keep_stats = "pd", eval_at_center = TRUE) |>
 #'   plot(num_points = TRUE)
 postprocess <- function(
   object,
-  choose_stats = c("obs", "pred", "pd"),
+  drop_stats = NULL,
   eval_at_center = FALSE,
+  collapse_m = Inf,
   drop_below_n = 0,
   drop_below_prop = 0,
   explicit_na = TRUE,
@@ -39,11 +41,12 @@ postprocess <- function(
     postprocess_one,
     x = object,
     eval_at_center = eval_at_center,
+    collapse_m = collapse_m,
     drop_below_n = drop_below_n,
     drop_below_prop = drop_below_prop,
     explicit_na = explicit_na,
     na.rm = na.rm,
-    MoreArgs = list(choose_stats = choose_stats),
+    MoreArgs = list(drop_stats = drop_stats),
     SIMPLIFY = FALSE
   )
   class(out) <- "marginal"
@@ -52,7 +55,8 @@ postprocess <- function(
 
 postprocess_one <- function(
   x,
-  choose_stats,
+  drop_stats,
+  collapse_m,
   eval_at_center,
   drop_below_n,
   drop_below_prop,
@@ -61,10 +65,11 @@ postprocess_one <- function(
   ...
 ) {
   num <- is.numeric(x$eval_at)
+  all_stats <- c("pred", "obs", "pd")
 
-  if (length(choose_stats) < 3L) {
-    vars <- setdiff(colnames(x), setdiff(c("obs", "pred", "pd"), choose_stats))
-    x <- x[vars]
+  if (!is.null(drop_stats)) {
+    stopifnot(drop_stats %in% all_stats)
+    x <- x[setdiff(colnames(x), drop_stats)]
   }
 
   if (num) {
@@ -74,6 +79,25 @@ postprocess_one <- function(
   }
 
   if (!num) {
+    if (collapse_m < nrow(x)) {
+      x_list <- split(x, order(x$exposure, decreasing = TRUE) < collapse_m)
+      x_keep <- x_list$`TRUE`
+      x_agg <- x_list$`FALSE`
+
+      # Prepare new factors
+      x_keep <- droplevels(x_keep)
+      lvl <- levels(x_keep$bar_at)
+      if ("Other" %in% lvl) {
+        stop("Factor level 'Other' already exists in 'bar_at'!")
+      }
+      levels(x_keep$bar_at) <- levels(x_keep$eval_at) <- c(lvl, "Other")
+
+      # Collapse other rows
+      S <- x_agg[intersect(colnames(x), all_stats)]
+      gS <- grouped_mean(S, g = rep.int(1, nrow(S)), w = x_agg$exposure)
+      x_new <- data.frame(bar_at = "Other", bar_width = 0.7, eval_at = "Other", gS)
+      x <- rbind(x_keep, x_new)  # Column order dof x_new oes not matter
+    }
     if (drop_below_n > 0) {
       x <- subset(x, exposure >= drop_below_n)
     }
@@ -91,12 +115,11 @@ postprocess_one <- function(
         x[s, c("bar_at", "eval_at")] <- "NA"
       }
     }
-    x <- droplevels(x)
   }
 
   if (isTRUE(na.rm)) {
     x <- subset(x, !is.na(bar_at))
   }
 
-  x
+  droplevels(x)
 }
