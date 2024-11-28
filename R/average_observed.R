ec74da634bd655e269972195637043c7dd676085
Michael Mayer <mayermichael79@gmail.com>
2024-11-28 17:35:17 +0100

    Increase default of discrete_m to 13

--- R/average_observed.R
+++ R/average_observed.R
@@ -32,7 +32,7 @@ average_observed <- function(
     x_name = "x",
     breaks = "Sturges",
     right = TRUE,
-    discrete_m = 5L,
+    discrete_m = 13L,
     outlier_iqr = 2,
     seed = NULL,
     ...
